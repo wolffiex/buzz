85010a1521fc76517fefe6a8a0531644bd1ec2a7
Adam Wolff <adam@electra>
2022-01-18 11:10:52 -0800

    Playing with base64 and random bits

--- src/buzz.rs
+++ src/buzz.rs
@@ -1,19 +1,42 @@
-use std::collections::HashSet;
+use ring::rand::SecureRandom;
+use base64::{encode};
+type ID = [u8; 16];
 
-type ID = &str;
+#[allow(dead_code)]
 enum Value {
     Null,
     Bool(bool),
-    Number(Number),
+    Number(i32),
     String(String),
     Id(ID),
 }
-type UPDATE = (
-    ID,
-    (Vec<(PROP_NAME, JSON_VALUE)>, Vec<EDGE_NAME, JSON_VALUE>),
-);
 
-pub fn write(id: ID, index: INDEX_NAME, props: &[&Value]) -> ID {}
-pub fn new_key() -> ID {}
+#[allow(dead_code)]
+pub struct Prop {
+    name: String,
+    value: Value,
+}
 
-pub fn read() -> ID {}
+#[allow(dead_code)]
+//pub fn read(id: ID, offset: i32) -> [u64] {
+#[allow(dead_code)]
+pub fn write(_id: ID, _props: &[&Prop]) -> ID {
+    let id = get_random_id();
+    return id;
+}
+
+fn get_random_id() -> ID {
+    let mut randoms: ID = [0; 16];
+
+    let sr = ring::rand::SystemRandom::new();
+    sr.fill(&mut randoms).expect("SystemRandom failure");
+    randoms
+}
+
+#[cfg(test)]
+#[test]
+fn test_it() {
+    let randoms = get_random_id();
+    println!("it's {:?}", randoms);
+    println!("encl {:?}", encode(randoms));
+}
