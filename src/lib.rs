6c3e8357eef68b7e2170d8faa2b7f560c8f7abe2
Adam Wolff <adam@electra>
2022-03-10 16:34:12 -0800

    Server scaffold

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
 type Id = [u8;8];
 
+type Topic = [u8;16];
+
+enum Value {
+    Str(String),
+    Num(i32),
+    Boo(bool),
+    Edg(Id),
+}
+
+struct Prop {
+    name: String,
+    value: Value,
+}
+
+struct Message {
+    topic: Topic,
+    id: Id,
+    props: Vec<Prop>
+}
+
 pub fn get_id() -> Id {
     [0;8]
+}
+
+
+#[cfg(test)]
+#[test]
+fn test_largest() {
+    assert_eq!(33, 22);
 }
