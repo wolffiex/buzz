7fab88d8e030ab73a88d91019052cae73be44805
Adam Wolff <adam@electra>
2022-03-31 09:51:32 -0700

    Temporary build directory

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use axum::{response::Html, routing::get, Router};
 use buzz;
+use std::env::temp_dir;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 use std::process::Command;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    const TEMP_DIR: &str = "FDLKJ.wasm";
+    let mut temp_dir = temp_dir();
+    temp_dir.push(format!("buzz-{}", Uuid::new_v4()));
+    println!("Build dir: {}", temp_dir.to_str().unwrap());
+    let shared_dir = Arc::new(Mutex::new(temp_dir));
+
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
-        .route("/wasm", get(move || wasm_handler(TEMP_DIR)));
+        .route("/wasm", get(move || wasm_handler(Arc::clone(&shared_dir))));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -25,12 +34,17 @@ async fn handler() -> Html<String> {
     Html(format!("Hello buzz {:?}", id).into())
 }
 
-async fn wasm_handler(out_dir: &str) -> Html<String> {
+async fn wasm_handler(dir_lock: Arc<Mutex<PathBuf>>) -> Html<String> {
+    let out_dir = dir_lock.lock().await;
+    let dir_name = out_dir.to_str().unwrap();
+    println!("drn {}", dir_name);
+    #[rustfmt::skip]
     let result = Command::new("cargo")
         .arg("build")
         .arg("--package").arg("wasm")
         .arg("--target").arg("wasm32-unknown-unknown")
-        .arg("--target-dir").arg(out_dir)
+        .arg("--target-dir").arg(dir_name)
+        .arg("--message-format").arg("json")
         .output();
     Html(format!("Hello wasm {:?}", result).into())
 }
