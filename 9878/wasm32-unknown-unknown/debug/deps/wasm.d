9d894054a77d21b7273a1d1ff4aaae5e31dfbfa9
Adam Wolff <adam@electra>
2022-04-16 11:38:27 -0700

    Oops, didn't mean to commit that directory

--- 9878/wasm32-unknown-unknown/debug/deps/wasm.d
+++
@@ -1,5 +0,0 @@
-/Users/adam/dev/buzz/9878/wasm32-unknown-unknown/debug/deps/wasm.wasm: wasm/src/lib.rs
-
-/Users/adam/dev/buzz/9878/wasm32-unknown-unknown/debug/deps/wasm.d: wasm/src/lib.rs
-
-wasm/src/lib.rs:
--- 9878/wasm32-unknown-unknown/debug/wasm.d
+++
@@ -1,1 +0,0 @@
-/Users/adam/dev/buzz/9878/wasm32-unknown-unknown/debug/wasm.wasm: /Users/adam/dev/buzz/wasm/src/lib.rs
